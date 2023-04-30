04f1656375b50e6eb38c1090eb8f0e9eb311d6e3
Saip-kun <saifsyafii22@gmail.com>
2023-04-30 12:03:27 +0700

    Bikin bikin theme and color

--- app/src/main/java/com/example/marketplace/NavigationActivity.kt
+++ app/src/main/java/com/example/marketplace/NavigationActivity.kt
@@ -33,7 +33,7 @@ class NavigationActivity : AppCompatActivity() {
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications, R.id.navigation_keranjang
             )
         )
-        setupActionBarWithNavController(navController, appBarConfiguration)
+//      setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
         navView.setOnItemSelectedListener {
 
