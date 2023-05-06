7fc78de7dc62d8183ede69cb41bc3fac536b922e
Saip-kun <saifsyafii22@gmail.com>
2023-05-06 21:13:54 +0700

    masih error pas ngerun

--- app/src/main/java/com/example/marketplace/util/MyApp.kt
+++ app/src/main/java/com/example/marketplace/util/MyApp.kt
@@ -7,7 +7,7 @@ import com.example.marketplace.core.di.viewModelModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
-class MyApp(activity: Application) {
+class MyApp : Application() {
     override fun onCreate() {
         super.onCreate()
         startKoin {
