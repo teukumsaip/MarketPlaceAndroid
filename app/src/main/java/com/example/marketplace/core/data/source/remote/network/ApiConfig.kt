54d6a5cf6114a530c77932e963060b35c75d6d4c
Saip-kun <saifsyafii22@gmail.com>
2023-05-06 21:56:31 +0700

    prebaikan fragment_home.xml

--- app/src/main/java/com/example/marketplace/core/data/source/remote/network/ApiConfig.kt
+++ app/src/main/java/com/example/marketplace/core/data/source/remote/network/ApiConfig.kt
@@ -10,7 +10,7 @@ import java.util.concurrent.TimeUnit
 
 object ApiConfig {
 
-    private const val BASE_URL = "http://127.0.0.1:8000/api/"
+    private const val BASE_URL = "https://127.0.0.1:8000/api/"
 
     private val client: Retrofit
         get() {
